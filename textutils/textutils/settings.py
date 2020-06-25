7a8fe8e68a2f5e24595d00643512b6b0388eba67
kritirikhi <kritirikhi1999@gmail.com>
2020-06-25 22:33:56 +0530

    added procfile for deployment

--- textutils/textutils/settings.py
+++ textutils/textutils/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/2.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
-
+import django_heroku
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -122,3 +122,4 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),'textutils/templates')
 
+django_heroku.settings(locals())
